b93afba394e395ec236f86a149ddda94ba074e8d
Nayannyk <99406754+Nayannyk@users.noreply.github.com>
2025-10-15 03:14:27 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,17 +13,3 @@ output "cluster_security_group_id" {
 output "node_group_role_arn" {
   value = module.eks.eks_managed_node_groups["default"].iam_role_arn
 }
-
-output "kubeconfig_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
-}
-
-output "kubeconfig" {
-  description = "Kubeconfig snippet that can be combined into a kubeconfig file."
-  value = module.eks.cluster_kubeconfig
-}
-
-output "node_group_names" {
-  value = keys(module.eks.node_groups)
-}
-

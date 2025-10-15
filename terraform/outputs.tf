baabbe6fb34bc36cb10a32b015513c7ae9de270e
Nayannyk <99406754+Nayannyk@users.noreply.github.com>
2025-10-15 02:47:32 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,17 @@
 output "cluster_name" {
-  value = module.eks.cluster_id
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "node_group_role_arn" {
+  value = module.eks.eks_managed_node_groups["default"].iam_role_arn
 }
 
 output "kubeconfig_certificate_authority_data" {

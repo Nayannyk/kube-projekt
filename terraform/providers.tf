d236970e232d85eb8db6a62d289cfcc90b2f444f
Nayannyk <99406754+Nayannyk@users.noreply.github.com>
2025-10-15 02:56:42 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,26 +1,14 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.5.0"
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
-
-# Kubernetes and Helm providers will be configured dynamically (after EKS cluster created)
-# using the outputs of the eks module in main.tf through provider aliases.
-
